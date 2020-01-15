856dd3a659fbc55f9bb7587583140f9402a81128
Elza Simões <elzaesimoes@gmail.com>
2020-01-15 11:34:03 -0400

    Conexão com BD

--- ConexaoBD/index.php
+++ ConexaoBD/index.php
@@ -3,8 +3,15 @@
 $servidor = "localhost";
 $usuario = "root";
 $senha = "mj123";
-$banco = "curso_mysqli1";
+$banco = "curso_myqsli";
 
-$conexao = @mysqli_connect ($servidor, $usuario, $servidor, $banco) or die ("Não foi possível fazer a conexão com o banco de dados")
+$conexao = @mysqli_connect($servidor, $usuario, $senha, $banco) or die ("Não foi possível fazer a conexão com o banco de dados");
 
-$qry =
+$qry = "insert into cliente(cliente, email, fone, endereco) values('Elza Simoes', 'elzaesimoes@gmail.com', 'rua 30' )";
+
+$resultado = $mysqli_query($conexao, $qry);
+
+if ($resultado)
+    echo "Operação realizada com sucesso";
+else
+    echo "Não foi possível";
