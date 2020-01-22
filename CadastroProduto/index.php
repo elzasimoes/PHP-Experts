4baca24ecd9642d50bdb794061b9492a461ff79f
Elza Simões <elzaesimoes@gmail.com>
2020-01-22 11:49:46 -0400

    index do sistema

---
+++ CadastroProduto/index.php
@@ -0,0 +1,22 @@
+<!doctype HTML>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Sistema de Produto</title>
+    </head>
+
+<body>
+        <h2> Curso de Cadastro de Produto </h2>
+            <ul>
+                <li><a href="">link1</a></li>
+                <li><a href="">link1</a></li>
+                <li><a href="">link1</a></li>
+                <li><a href="">link1</a></li>
+                <li><a href="">link1</a></li>
+                <li><a href="">link1</a></li>
+                <li><a href="">link1</a></li>
+            <ul>
+<body>
+
+</html>
