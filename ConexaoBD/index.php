8b0dff7711d59438cb57de1145a505fa33039d3d
Elza Simões <elzaesimoes@gmail.com>
2020-01-22 09:42:15 -0400

    corrigindo erro do insert

--- ConexaoBD/index.php
+++ ConexaoBD/index.php
@@ -1,17 +1,19 @@
 <?php
 
-$servidor = "localhost";
+$servidor = "127.0.0.1";
 $usuario = "root";
-$senha = "mj123";
-$banco = "curso_myqsli";
+$senha = "";
+$banco = "curso_mysqli";
 
 $conexao = @mysqli_connect($servidor, $usuario, $senha, $banco) or die ("Não foi possível fazer a conexão com o banco de dados");
 
-$qry = "insert into cliente(cliente, email, fone, endereco) values('Elza Simoes', 'elzaesimoes@gmail.com', 'rua 30' )";
+$qry = "INSERT INTO cliente(cliente, email, fone, endereco) VALUES('Rayane', 'fulano@gmail.com', 'rua 49,50','7777-7777')";
 
-$resultado = $mysqli_query($conexao, $qry);
+$resultado = @mysqli_query($conexao, $qry);
 
 if ($resultado)
     echo "Operação realizada com sucesso";
 else
     echo "Não foi possível";
+
+
