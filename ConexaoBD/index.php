fdab66139d9bb1fc3ca7fe4dc6122ada57a869a9
Elza Simões <elzaesimoes@gmail.com>
2020-01-22 09:52:08 -0400

    Excluindo dados do BD

--- ConexaoBD/index.php
+++ ConexaoBD/index.php
@@ -9,8 +9,9 @@ $conexao = @mysqli_connect($servidor, $usuario, $senha, $banco) or die ("Não fo
 
 $qryInsert = "INSERT INTO cliente(cliente, email, fone, endereco) VALUES('Rayane', 'fulano@gmail.com', 'rua 49,50','7777-7777')";
 $qryUpdate = "UPDATE cliente set endereco = 'rua tal tal 58' where id_cliente = 2";
+$qryDelete = "DELETE FROM `cliente` WHERE id_cliente = 3";
 
-$resultado = @mysqli_query($conexao, $qryUpdate);
+$resultado = @mysqli_query($conexao, $qryDelete);
 
 if ($resultado)
     echo "Operação realizada com sucesso";
